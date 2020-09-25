2249ef4fd727bde8b3c3ebefeb4ebc2c33dc16db
Thảo <thaold@kikaitech.io>
2020-09-25 13:27:59 +0700

    preset-stage-2 file-loader load jqurey

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import { createStore, applyMiddleware } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
-)
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
     entry: {
@@ -21,8 +22,20 @@ module.exports = {
                     'css-loader'
                 ],
                 test: /\.css$/
+            },
+            {
+                loader: 'file-loader',
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$|\.ico$/
             }
         ]
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            "$": 'jquery',
+            "jQuery": 'jquery',
+            "window.$": 'jquery',
+            "window.jQuery": 'jquery',
+        })
+    ],
     mode: 'development'
 }
